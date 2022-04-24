f3ddd5972c0cbfce31b73cee6c3197fd361a65ea
Christopher Homberger <christopher.homberger@web.de>
2022-04-24 16:47:11 +0200

    fix: act-*-env volumes not removed

--- pkg/runner/run_context.go
+++ pkg/runner/run_context.go
@@ -286,7 +286,7 @@ func (rc *RunContext) stopJobContainer() common.Executor {
 	return func(ctx context.Context) error {
 		if rc.JobContainer != nil && !rc.Config.ReuseContainers {
 			return rc.JobContainer.Remove().
-				Then(container.NewDockerVolumeRemoveExecutor(rc.jobContainerName(), false).If(func(ctx context.Context) bool { return !rc.Local }))(ctx)
+				Then(container.NewDockerVolumeRemoveExecutor(rc.jobContainerName()+"-env", false).If(func(ctx context.Context) bool { return !rc.Local }))(ctx)
 		}
 		return nil
 	}
